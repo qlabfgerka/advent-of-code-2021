eebe67cbeb61477ebfe0f587367e2983724ae424
qlabfgerka <71296359+qlabfgerka@users.noreply.github.com>
2021-12-03 19:41:21 +0100

    Day 3 - Part 2
    Day 3 - Part 2 and README updates

--- day-3/main.cpp
+++ day-3/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,10 +11,48 @@ int help() {
     return -1;
 }
 
+void erase(vector<string> &binaryNumbers, int index, char value) {
+    binaryNumbers.erase(std::remove_if(
+            binaryNumbers.begin(), binaryNumbers.end(), [index, value](const string &x) {
+                return x[index] != value;
+            }), binaryNumbers.end());
+}
+
+int toDecimal(const string &binary) {
+    uint num = 0;
+    for (int i = 0; i < binary.size(); i++) {
+        num ^= (-(binary[i] == '1' ? 1UL : 0UL) ^ num) & (1UL << (binary.size() - 1 - i));
+    }
+
+    return num;
+}
+
+string criteria(vector<string> array, vector<int> ones, char value) {
+    int counter = 0;
+    while (true) {
+        ones[counter] = 0;
+        for (int i = 0; i < array.size(); i++) {
+            if (array[i][counter] == '1') {
+                ones[counter] = ones[counter] + 1;
+            }
+        }
+
+        if (ones[counter] >= array.size() - ones[counter]) erase(array, counter, value);
+        else erase(array, counter, value == '1' ? '0' : '1');
+
+        if (array.size() == 1) break;
+
+        counter++;
+    }
+
+    return array[0];
+}
+
 int binaryDiagnostic(const string &filename) {
     ifstream file(filename);
     string binaryNumber;
     vector<int> ones;
+    vector<string> binaryNumbers;
     int counter = 0;
     uint gamma = 0, epsilon = 0;
     bool first = true;
@@ -23,6 +62,8 @@ int binaryDiagnostic(const string &filename) {
     }
 
     while (file >> binaryNumber) {
+        binaryNumbers.push_back(binaryNumber);
+
         if (first) {
             ones = vector<int>(binaryNumber.size(), 0);
             first = false;
@@ -38,13 +79,15 @@ int binaryDiagnostic(const string &filename) {
 
     for (int i = 0; i < ones.size(); i++) {
         if (ones[i] > counter - ones[i]) {
-            cout << "1";
             gamma ^= (-1UL ^ gamma) & (1UL << (ones.size() - 1 - i));
         } else {
             epsilon ^= (-1UL ^ epsilon) & (1UL << (ones.size() - 1 - i));
         }
     }
-    cout << gamma << " " << epsilon << " " << gamma * epsilon << endl;
+
+    cout << gamma * epsilon << " " <<
+         toDecimal(criteria(binaryNumbers, ones, '1')) *
+         toDecimal(criteria(binaryNumbers, ones, '0')) << endl;
 
     return 0;
 }
