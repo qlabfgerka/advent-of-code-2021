5942f83edeed585d36c2d5a77c4854dfb9f3ef77
qlabfgerka <71296359+qlabfgerka@users.noreply.github.com>
2021-12-09 14:46:55 +0100

    Day 9 - Part 2
    --- Part Two ---
    
    Next, you need to find the largest basins so you know what areas are most important to avoid.
    
    A basin is all locations that eventually flow downward to a single low point. Therefore, every low point has a basin, although some basins are very small. Locations of height 9 do not count as being in any basin, and all other locations will always be part of exactly one basin.
    
    The size of a basin is the number of locations within the basin, including the low point. The example above has four basins.
    
    The top-left basin, size 3:
    
    2199943210
    3987894921
    9856789892
    8767896789
    9899965678
    The top-right basin, size 9:
    
    2199943210
    3987894921
    9856789892
    8767896789
    9899965678
    The middle basin, size 14:
    
    2199943210
    3987894921
    9856789892
    8767896789
    9899965678
    The bottom-right basin, size 9:
    
    2199943210
    3987894921
    9856789892
    8767896789
    9899965678
    Find the three largest basins and multiply their sizes together. In the above example, this is 9 * 14 * 9 = 1134.
    
    What do you get if you multiply together the sizes of the three largest basins?

--- day-9/main.cpp
+++ day-9/main.cpp
@@ -20,11 +20,45 @@ bool lowPoint(vector<vector<int>> smoke, int i, int j) {
             smoke[i][j] < getValue(smoke, i, j + 1) && smoke[i][j] < getValue(smoke, i + 1, j) != 0);
 }
 
-int lavaTubes(const string &filename) {
+void mark(vector<vector<bool>> &checked, int i, int j, int &size) {
+    checked[i][j] = true;
+    ++size;
+}
+
+int basins(vector<vector<int>> &smoke, vector<vector<bool>> &checked, int i, int j) {
+    int size = 0;
+    ++size;
+    checked[i][j] = true;
+
+    if (getValue(smoke, i - 1, j) < 9 && !checked[i - 1][j]) size += basins(smoke, checked, i - 1, j);
+    if (getValue(smoke, i, j - 1) < 9 && !checked[i][j - 1]) size += basins(smoke, checked, i, j - 1);
+    if (getValue(smoke, i, j + 1) < 9 && !checked[i][j + 1]) size += basins(smoke, checked, i, j + 1);
+    if (getValue(smoke, i + 1, j) < 9 && !checked[i + 1][j]) size += basins(smoke, checked, i + 1, j);
+
+    if (getValue(smoke, i - 1, j) < 9 && !checked[i - 1][j]) mark(checked, i - 1, j, size);
+    if (getValue(smoke, i, j - 1) < 9 && !checked[i][j - 1]) mark(checked, i, j - 1, size);
+    if (getValue(smoke, i, j + 1) < 9 && !checked[i][j + 1]) mark(checked, i, j + 1, size);
+    if (getValue(smoke, i + 1, j) < 9 && !checked[i + 1][j]) mark(checked, i + 1, j, size);
+
+    return size;
+}
+
+void initializeChecked(vector<vector<bool>> &checked, int n, int m) {
+    for (int i = 0; i < n; i++) {
+        checked.emplace_back();
+        for (int j = 0; j < m; j++) {
+            checked[i].push_back(false);
+        }
+    }
+}
+
+int lavaTubes(const string &filename, bool type) {
     ifstream file(filename);
     int counter = 0;
     string line;
     vector<vector<int>> smoke;
+    vector<vector<bool>> checked;
+    vector<int> basinsVector;
 
     if (!file.is_open()) {
         return help();
@@ -43,17 +77,29 @@ int lavaTubes(const string &filename) {
     counter = 0;
     for (int i = 0; i < smoke.size(); i++) {
         for (int j = 0; j < smoke[i].size(); j++) {
-            if (lowPoint(smoke, i, j)) counter += smoke[i][j] + 1;
-
+            if (lowPoint(smoke, i, j)) {
+                if (!type) counter += smoke[i][j] + 1;
+                else {
+                    initializeChecked(checked, smoke.size(), smoke[0].size());
+                    checked[i][j] = true;
+                    basinsVector.push_back(basins(smoke, checked, i, j));
+                }
+            }
         }
     }
 
-    cout << counter << endl;
+    if (!type) cout << counter << endl;
+    else {
+        sort(basinsVector.begin(), basinsVector.end(), greater<int>());
+        cout << basinsVector[0] * basinsVector[1] * basinsVector[2] << endl;
+    }
+
+
     return 0;
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 2) return help();
 
-    return lavaTubes(argv[1]);
+    return lavaTubes(argv[1], true);
 }
