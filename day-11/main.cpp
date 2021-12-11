c3a799c0eaafcc263881d9f07ca08904f2de8918
qlabfgerka <71296359+qlabfgerka@users.noreply.github.com>
2021-12-11 20:46:06 +0100

    Day 11 - Part 2
    --- Part Two ---
    
    It seems like the individual flashes aren't bright enough to navigate. However, you might have a better option: the flashes seem to be synchronizing!
    
    In the example above, the first time all octopuses flash simultaneously is step 195:
    
    After step 193:
    5877777777
    8877777777
    7777777777
    7777777777
    7777777777
    7777777777
    7777777777
    7777777777
    7777777777
    7777777777
    
    After step 194:
    6988888888
    9988888888
    8888888888
    8888888888
    8888888888
    8888888888
    8888888888
    8888888888
    8888888888
    8888888888
    
    After step 195:
    0000000000
    0000000000
    0000000000
    0000000000
    0000000000
    0000000000
    0000000000
    0000000000
    0000000000
    0000000000
    If you can calculate the exact moments when the octopuses will all flash simultaneously, you should be able to navigate through the cavern. What is the first step during which all octopuses flash?

--- day-11/main.cpp
+++ day-11/main.cpp
@@ -15,8 +15,7 @@ int flash(vector<vector<int>> &octopuses, vector<vector<bool>> &flashed, int i, 
 
     int flashes = 0;
 
-    if (!flashed[i][j])
-        ++octopuses[i][j];
+    if (!flashed[i][j]) ++octopuses[i][j];
 
     if (octopuses[i][j] > 9) {
         octopuses[i][j] = 0;
@@ -37,16 +36,35 @@ int flash(vector<vector<int>> &octopuses, vector<vector<bool>> &flashed, int i, 
     return flashes;
 }
 
-void setFlashed(vector<vector<bool>> &flashed) {
+void helper(vector<vector<int>> &octopuses, vector<vector<bool>> flashed, int &flashes) {
     for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) flashed[i][j] = false;
+        for (int j = 0; j < 10; j++) {
+            flashes += flash(octopuses, flashed, i, j);
+        }
+    }
+}
+
+int countFlashes(vector<vector<int>> octopuses, const vector<vector<bool>> &flashed) {
+    int flashes = 0;
+    for (int iterations = 0; iterations < 100; iterations++) {
+        helper(octopuses, flashed, flashes);
+    }
+    return flashes;
+}
+
+int getStep(vector<vector<int>> octopuses, const vector<vector<bool>> &flashed) {
+    int flashes, step = 0;
+    while (true) {
+        flashes = 0;
+        ++step;
+        helper(octopuses, flashed, flashes);
+        if (flashes == 100) return step;
     }
 }
 
 int dumboOctopus(const string &filename) {
     fstream file(filename);
     string line;
-    int flashes = 0;
     vector<vector<int>> octopuses(10, vector<int>(10, 0));
     vector<vector<bool>> flashed(10, vector<bool>(10, false));
 
@@ -61,16 +79,8 @@ int dumboOctopus(const string &filename) {
         }
     }
 
-    for (int iterations = 0; iterations < 100; iterations++) {
-        setFlashed(flashed);
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                flashes += flash(octopuses, flashed, i, j);
-            }
-        }
-    }
-    
-    cout << flashes << endl;
+    cout << countFlashes(octopuses, flashed) << endl;
+    cout << getStep(octopuses, flashed) << endl;
 
     return 0;
 }
